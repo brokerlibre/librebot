3ea0a3a754b13bf7caecdab18934c1e7d40bbf17
ArthurTemporim <arthurrtl@gmail.com>
2019-02-16 09:57:00 -0200

    Add integration with api
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -1,9 +1,32 @@
 from rasa_core_sdk import Action
 from rasa_core_sdk.events import SlotSet
 
+import requests
+
+#-------------------------------------------
+
+def get_data(complement=''):
+    response = 'Connection problem :/'
+    try:
+        response = requests.get('https://libreapi.temposerver.ml/api/{}'.format(complement))
+    except ValueError:
+        print(ValueError)
+    return response.json()
+
+def get_names(all_data):
+    list = ''
+    for data in all_data:
+       list += data['name'] 
+       list += '\n'
+    return list
+
+#-------------------------------------------
+
 class ActionTest(Action):
    def name(self):
       return "action_test"
 
    def run(self, dispatcher, tracker, domain):
-      dispatcher.utter_message("Esta é uma mensagem customizada!")
+      response = get_data('customer')
+      names = get_names(response)
+      dispatcher.utter_message(names)
